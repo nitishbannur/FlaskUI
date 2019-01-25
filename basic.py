e1baa4eebbfada96338e5a865567de210e6494c0
Nitish Bannur <30689224+nitishmcfc@users.noreply.github.com>
2019-01-25 16:23:33 +0530

    Email Part Works

--- basic.py
+++ basic.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_mail import Mail, Message
+app =Flask(__name__)
+mail=Mail(app)
+
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = '@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+
+
+#@app.route("/")
+#def index():
+  # return render_template("index.html")
 
 @app.route("/")
 def index():
-   return render_template("index.html")
+   msg = Message('Hello', sender = 'sender email id', recipients = ['write receiver email'])
+   msg.body = "HI THIS IS A SAMPLE MAIL"
+   mail.send(msg)
+   return "Sent"
 
 if __name__ == '__main__':
    app.run(debug = True)
