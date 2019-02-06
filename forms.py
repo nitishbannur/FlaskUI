6943ddbc5827455afc68dc06dddb5c141a94fbe1
Anurag Katakkar <anuraglkatakkar@gmail.com>
2019-02-07 01:10:59 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField, TextAreaField
-from wtforms.widgets import TextArea
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -18,8 +17,16 @@ class LoginForm(FlaskForm):
 class HomePageForm(FlaskForm):
 	name = StringField('Victim Name', validators=[DataRequired()])
 	crime_type = SelectField('Crime to Report', choices=[('theft','Theft'),('assault','Assault'),('gta','Grand Theft Auto'),('sha','Sexual Harrassment'),('other', 'Other')])
-	description = StringField('Facial Features of Suspect', widget=TextArea())
+	description = TextAreaField('Facial Features of Suspect')
 	generate_img = SubmitField('Generate Images')
 	incident_report = TextAreaField('Incident Report')
 	submit_report = SubmitField('Submit Report')
-	select_img=BooleanField()
+	
+	select_img1 = BooleanField()
+	select_img2 = BooleanField()
+	select_img3 = BooleanField()
+	select_img4 = BooleanField()
+	select_img5 = BooleanField()
+	select_img6 = BooleanField()
+	select_img7 = BooleanField()
+	select_img8 = BooleanField()
